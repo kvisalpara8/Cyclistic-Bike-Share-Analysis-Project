d5247d3d14ad560d68a37659d4a8a577a6474ac0
Krish Visalpara <krishvisalpara2004@gmail.com>
2024-08-18 19:19:22 +0530

    Update 2_number of trip by usertype and month.sql

--- Cyclistic Bike-Share Capstone Project/sql query/2_number of trip by usertype and month.sql
+++ Cyclistic Bike-Share Capstone Project/sql query/2_number of trip by usertype and month.sql
@@ -1,6 +1,6 @@
 SELECT
     usertype,
-    TO_CHAR(start_time::TIMESTAMP, 'Month') AS months,
+    months,
     COUNT(usertype) AS total_trips,
     CONCAT(CAST(COUNT(usertype) * 100/ SUM(COUNT(usertype)) OVER() AS DECIMAL(10,2)), '%') AS trip_percentage
 FROM
@@ -9,5 +9,5 @@ GROUP BY
     months,
     usertype
 ORDER BY
-    min(start_time),
+    months,
     usertype
